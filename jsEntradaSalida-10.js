f2f67def80aa59e1d7eb0c0e2d807c90a25d1a09
juanneves99 <juanneves99@hotmail.com>
2017-02-03 16:25:18 -0300

    Add files via upload

---
+++ jsEntradaSalida-10.js
@@ -0,0 +1,16 @@
+/*Debemos lograr tomar el importe por ID.
+Transformarlo a entero (parseInt), luego
+mostrar el importe con un Descuento del 25 %
+en el cuadro de texto "RESULTADO"*/
+function MostrarAumento()
+{
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+	numeroUno=document.getElementById('importe').value;
+	numeroDos=document.getElementById('resultado').value;
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+	resultado=
+	alert(resultado);
+}
---
+++ jsEntradaSalida-9.js
@@ -0,0 +1,17 @@
+/*Debemos lograr tomar el importe por ID ,
+transformarlo a entero (parseInt), luego
+mostrar el importe con un aumento del 10 %
+en el cuadro de texto "RESULTADO".*/
+function MostrarAumento()
+{
+    var numeroUno;
+    var numeroDos;
+    var resultado;
+    numeroUno=document.getElementById('sueldo').value;
+    numeroDos=document.getElementById('resultado').value;
+    numeroUno=parseInt(numeroUno);
+    numeroDos=parseInt(numeroDos);
+    resultado=numeroUnonumeroDos;
+    alert(resultado);
+	
+}
