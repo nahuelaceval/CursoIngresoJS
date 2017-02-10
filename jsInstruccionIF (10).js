dc58b84b8ff9492fd286e9023f4c35ca379de366
nahuelaceval <nahuel.acevalr@gmail.com>
2017-02-10 09:08:17 -0300

    Add files via upload

--- jsInstruccionIF (10).js
+++ jsInstruccionIF (10).js
@@ -2,5 +2,23 @@ function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+NumeroRandom=Math.floor((Math.random() * 10) + 1);
 
+
+		if(NumeroRandom>8)
+		{
+			alert(NumeroRandom + " EXCELENTE");
+		}
+		
+		
+		if(NumeroRandom<8&&NumeroRandom>3)
+		{
+			alert(NumeroRandom + " Aprobado");
+		}
+				
+		
+		if(NumeroRandom<4)
+		{
+			alert(NumeroRandom + " Vamos, la próxima se puede");
+		}
 }//FIN DE LA FUNCIÓN
