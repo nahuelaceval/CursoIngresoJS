e11c9e6b567be53ab37cb4f2107c15bb93c5a484
nahuelaceval <nahuel.acevalr@gmail.com>
2017-02-23 19:21:51 -0300

    Add files via upload

--- jsIteraciones (8).js
+++ jsIteraciones (8).js
@@ -4,30 +4,9 @@ function Mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
-	var resultado;
+	
 	var respuesta='si';
-	var numero;
 
-	while(respuesta=="si")
-
-	{	numero=prompt("Ingrese su numero");
-		numero=parseInt(numero);
-		if(numero>0)
-		{
-			positivo=(numero+numero);
-
-		}
-		else if(numero<0)
-		{
-
-			negativo=(numero*numero);
-		}
-		
-		
-		contador=contador+1;
-		respuesta=prompt("ingrese si para continuar");
-
-	}
 
 document.getElementById('suma').value=positivo;
 document.getElementById('producto').value=negativo;
